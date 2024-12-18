305c7f226bd72aeaf0fa2dba68512b06aeb6d157
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-18 20:05:06 -0300

    Updates na infra

--- FinTrack.Infrastructure/Persistance/unitOfWork/UoF.cs
+++ FinTrack.Infrastructure/Persistance/unitOfWork/UoF.cs
@@ -1,21 +1,32 @@
-﻿using FinTrack.Core.UnitOfWork;
+﻿using FinTrack.Core.Repositories;
+using FinTrack.Core.UnitOfWork;
 using FinTrack.Infrastructure.Persistance;
+using FinTrack.Infrastructure.Persistance.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FinTrack.Infrastructure.UnitOfWork
+namespace FinTrack.Infrastructure.Persistance.unitOfWork
 {
     public class UoF : IUoF
     {
         private readonly FinTrackDbContext _context;
 
+        private IUserRepository _userRepository;
         public UoF(FinTrackDbContext context)
         {
             _context = context;
         }
+
+        public IUserRepository UserRepository
+        {
+            get
+            {
+                return _userRepository = _userRepository ?? new UserRepository(_context);
+            }
+            }
 
         public void Commit()
         {
