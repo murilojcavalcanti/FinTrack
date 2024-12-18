dc6e6ad3850fa5ac600fdc276b8cb62c4551b52e
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-18 19:04:24 -0300

    Criando interface do repositorio

---
+++ FinTrack.Core/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Core.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Get(Expression<Func<T,bool>>predicate);
+        Task<T> Insert(T Entity);
+        Task Update(T Entity);
+    }
+}
