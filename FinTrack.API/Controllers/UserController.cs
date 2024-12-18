62638fe03b84691d04a9c9d04966a94e8b7fb513
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-18 20:05:47 -0300

    controller de usuario

---
+++ FinTrack.API/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinTrack.API.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class UserController:ControllerBase
+    {
+
+    }
+}
