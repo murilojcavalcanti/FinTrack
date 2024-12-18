2b13f18ca5bfbe6f8b54018bc02563e71cacb213
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-18 19:06:07 -0300

    criando unitofwork

---
+++ FinTrack.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using FinTrack.Core.UnitOfWork;
+using FinTrack.Infrastructure.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace FinTrack.Infrastructure.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly FinTrackDbContext _context;
+
+        public UnitOfWork(FinTrackDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
