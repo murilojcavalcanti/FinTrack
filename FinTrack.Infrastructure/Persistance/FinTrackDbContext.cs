f787e3619c3e58aca21269a6e8f6fe94bae3528d
murilojcavalcanti <josemurilo7@gmail.com>
2024-12-18 19:04:37 -0300

    Criando Context

---
+++ FinTrack.Infrastructure/Persistance/FinTrackDbContext.cs
@@ -0,0 +1,17 @@
+﻿using FinTrack.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Infrastructure.Persistance
+{
+    public class FinTrackDbContext:DbContext
+    {
+        public FinTrackDbContext(DbContextOptions<FinTrackDbContext> opts):base(opts) { }
+
+        public User Users { get; set; }
+    }
+}
