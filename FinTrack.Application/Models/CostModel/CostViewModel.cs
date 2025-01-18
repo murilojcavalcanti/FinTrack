4cc3979ec8ba4253d9b0aa9431611f812ba86b96
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-18 19:54:01 -0300

    Criando Result View Model padrão e view model de Cost

---
+++ FinTrack.Application/Models/CostModel/CostViewModel.cs
@@ -0,0 +1,25 @@
+﻿using FinTrack.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Models.CostModel
+{
+    public class CostViewModel
+    {
+        public CostViewModel(decimal priceCost, string description, DateTime createdAt)
+        {
+            PriceCost = priceCost;
+            Description = description;
+            CreatedAt = createdAt;
+        }
+
+        public decimal PriceCost { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public static CostViewModel FromEntity(Cost Entity) => new(Entity.PriceCost, Entity.Description, Entity.CreatedAt);
+    }
+}
---
+++ FinTrack.Application/Models/ResultViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace FinTrack.Application.Models;
+public class ResultViewModel
+{
+    public ResultViewModel(bool isSuccess = true, string messsage = "")
+    {
+        IsSuccess = isSuccess;
+        Messsage = messsage;
+    }
+
+    public bool IsSuccess { get; private set; }
+    public string Messsage { get; private set; }
+    public static ResultViewModel Success() => new();
+    public static ResultViewModel Error(string message) => new(false, message);
+}
+
+public class ResultViewModel<T> : ResultViewModel
+{
+    public T Data { get; private set; }
+
+    public ResultViewModel(T data, bool isSuccess = true, string messsage = "")
+    {
+        Data = data;
+    }
+    public static ResultViewModel<T> Success(T Data) => new(Data);
+    public static ResultViewModel<T> Error(string message)
+        => new(default, false, message);
+}
