6e6d1d98581c21aa9c278dc7f1204edcccf38b7d
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-18 19:54:17 -0300

    Criando InserCommand

---
+++ FinTrack.Application/Services/Commands/CreateCost/CreateCostCommand.cs
@@ -0,0 +1,28 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Commands.CreateCost
+{
+    public class CreateCostCommand:IRequest<ResultViewModel<int>>
+    {
+        public CreateCostCommand(decimal priceCost, string description, DateTime createdAt)
+        {
+            PriceCost = priceCost;
+            Description = description;
+            CreatedAt = createdAt != DateTime.Now ? createdAt: DateTime.Now;
+        }
+
+        public decimal PriceCost { get; set; }
+        public string Description { get; set; }
+        DateTime CreatedAt { get; set; }
+
+        public Cost ToEntity() => new(CreatedAt,PriceCost, Description);
+    }
+}
