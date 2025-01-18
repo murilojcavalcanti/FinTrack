446652a85120d68a06d928b0d317b6ac299f5e3c
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-18 19:54:48 -0300

    Criando Controller

---
+++ FinTrack.API/Controllers/CostController.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinTrack.API.Controllers
+{
+    [Route("Api/Cost")
+    [ApiController]
+
+    public class CostController
+    {
+        private readonly IMediator _mediator;
+
+        public CostController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+
+    }
+}
