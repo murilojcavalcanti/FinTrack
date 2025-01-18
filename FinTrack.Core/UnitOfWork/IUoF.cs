e2bba0d9941d28d64d7a7fb55d71c401d4e1a6d2
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-18 19:49:41 -0300

    Adicionando a interface De repository

--- FinTrack.Core/UnitOfWork/IUoF.cs
+++ FinTrack.Core/UnitOfWork/IUoF.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FinTrack.Core.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,10 @@ using System.Threading.Tasks;
 
 namespace FinTrack.Core.UnitOfWork
 {
+
     public interface IUoF
     {
+        public ICostRepository CostRepository { get;}
         void Commit();
     }
 }
