44ed9d45757af518af82b709aa19a94ac14d0643
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-18 19:48:53 -0300

    Criando a interface do CostRepository

---
+++ FinTrack.Core/Repositories/ICostRepository.cs
@@ -0,0 +1,13 @@
+﻿using FinTrack.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Core.Repositories
+{
+    public interface ICostRepository: IRepository<Cost>
+    {
+    }
+}
