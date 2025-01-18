7f01a8cb2d804c3a983fd0dacaf23cd65a77eac1
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-18 19:49:07 -0300

    Criando CostRepository

---
+++ FinTrack.Infrastructure/Persistance/Repository/CostRepository.cs
@@ -0,0 +1,17 @@
+﻿using FinTrack.Core.Entities;
+using FinTrack.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Infrastructure.Persistance.Repository
+{
+    public class CostRepository : Repository<Cost>, ICostRepository
+    {
+        public CostRepository(FinTrackDbContext context) : base(context)
+        {
+        }
+    }
+}
