d48f890fecf0edc5c77343a8298d5099ba405d8c
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-20 15:02:25 -0300

    Criaando controller

--- FinTrack.API/Controllers/CostController.cs
+++ FinTrack.API/Controllers/CostController.cs
@@ -1,15 +1,18 @@
 ﻿
 
 
+using FinTrack.Application.Services.Commands.CreateCost;
 using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
 
 namespace FinTrack.API.Controllers
 {
-    [Route("Api/Cost")
+    [Route("Api/Cost")]
     [ApiController]
 
-    public class CostController
+    public class CostController : ControllerBase
     {
         private readonly IMediator _mediator;
 
@@ -18,7 +21,44 @@ namespace FinTrack.API.Controllers
             _mediator = mediator;
         }
 
-        [HttpGet]
+        [HttpPost]
+        public async Task<IActionResult> CreateCost(CreateCostCommand command)
+        {
+            var result = await _mediator.Send(command);
+            if (!result.IsSuccess) return BadRequest(result.Messsage);
 
+            return Ok(result);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCost()
+        {
+            return Ok();
+        }
+        [HttpGet("/GetById")]
+        public async Task<IActionResult> GetByIdCost()
+        {
+            return Ok();
+        }
+        [HttpGet("/GetById")]
+        public async Task<IActionResult> GetByDateCost()
+        {
+            return Ok();
+        }
+        [HttpGet("/GetByDescription")]
+        public async Task<IActionResult> GetByDescritionCost()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCost()
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCost()
+        {
+            return Ok();
+        }
     }
 }
