9c4719893dcd89775d282886e826a60bb6ca03d9
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-20 14:59:33 -0300

    Criando commandUpdate

---
+++ FinTrack.Application/Services/Commands/UpdateCost/UpdateCommand.cs
@@ -0,0 +1,25 @@
+﻿using FinTrack.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Commands.UpdateCost
+{
+    public class UpdateCommand:IRequest<ResultViewModel>
+    {
+        public UpdateCommand(decimal priceCost, string description, int idCost)
+        {
+
+            PriceCost = priceCost;
+            DescriptionCost = description;
+            IdCost = idCost;
+        }
+
+        public int IdCost { get; set; }
+        public decimal PriceCost { get; set; }
+        public string DescriptionCost { get; set; }
+    }
+}
---
+++ FinTrack.Application/Services/Commands/UpdateCost/UpdateHandler.cs
@@ -0,0 +1,30 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Application.Services.Commands.DeleteCost;
+using FinTrack.Core.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Commands.UpdateCost
+{
+    public class UpdateHandler : IRequestHandler<UpdateCommand, ResultViewModel>
+    {
+        private readonly IUoF _Uof;
+
+        public UpdateHandler(IUoF uof)
+        {
+            _Uof = uof;
+        }
+        public async Task<ResultViewModel> Handle(UpdateCommand request, CancellationToken cancellationToken)
+        {
+            var Cost = _Uof.CostRepository.Get(c => c.Id == request.IdCost);
+            if (Cost.Result is null) return ResultViewModel.Error("Cost Not Found!");
+            Cost.Result.Update(request.PriceCost,request.DescriptionCost);
+            _Uof.CostRepository.Update(Cost.Result);
+            return ResultViewModel.Success();
+        }
+    }
+}
