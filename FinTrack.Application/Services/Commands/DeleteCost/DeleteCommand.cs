cd295e994120536c399ae141e76335f04c6fb47e
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-20 14:59:19 -0300

    Criando CommandDelete

---
+++ FinTrack.Application/Services/Commands/DeleteCost/DeleteCommand.cs
@@ -0,0 +1,20 @@
+﻿using FinTrack.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Commands.DeleteCost
+{
+    public class DeleteCommand:IRequest<ResultViewModel>
+    {
+        public DeleteCommand(int costId)
+        {
+            CostId = costId;
+        }
+        public int CostId { get; set; }
+
+    }
+}
---
+++ FinTrack.Application/Services/Commands/DeleteCost/DeleteHandler.cs
@@ -0,0 +1,31 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Core.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Commands.DeleteCost
+{
+    public class DeleteHandler : IRequestHandler<DeleteCommand, ResultViewModel>
+    {
+        private readonly IUoF _Uof;
+
+        public DeleteHandler(IUoF uof)
+        {
+            _Uof = uof;
+        }
+
+
+        public async Task<ResultViewModel> Handle(DeleteCommand request, CancellationToken cancellationToken)
+        {
+            var Cost = _Uof.CostRepository.Get(c=>c.Id==request.CostId);
+            if (Cost.Result is null) return ResultViewModel.Error("Cost Not Found!");
+            Cost.Result.setAsDeleted();
+            _Uof.CostRepository.Update(Cost.Result);
+            return ResultViewModel.Success();
+        }
+    }
+}
