f54c1f48a14974ca570c1206850289a285c28208
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-22 22:41:40 -0300

    BugFix- Excluindo CreatedAt

---
+++ FinTrack.Application/Services/Commands/CostCommands/CreateCost/CreateCostCommand.cs
@@ -0,0 +1,26 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Commands.CostCommands.CreateCost
+{
+    public class CreateCostCommand : IRequest<ResultViewModel<int>>
+    {
+        public CreateCostCommand(decimal priceCost, string description)
+        {
+            PriceCost = priceCost;
+            Description = description;
+        }
+
+        public decimal PriceCost { get; set; }
+        public string Description { get; set; }
+
+        public Cost ToEntity() => new(PriceCost, Description);
+    }
+}
---
+++ FinTrack.Application/Services/Commands/CostCommands/CreateCost/CreateCostHandler.cs
@@ -0,0 +1,30 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Core.Repositories;
+using FinTrack.Core.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Commands.CostCommands.CreateCost
+{
+    public class CreateCostHandler : IRequestHandler<CreateCostCommand, ResultViewModel<int>>
+    {
+        private readonly IUoF _UoF;
+
+        public CreateCostHandler(IUoF uoF)
+        {
+            _UoF = uoF;
+        }
+
+        public async Task<ResultViewModel<int>> Handle(CreateCostCommand request, CancellationToken cancellationToken)
+        {
+            var cost = request.ToEntity();
+            await _UoF.CostRepository.Insert(cost);
+            _UoF.Commit();
+            return ResultViewModel<int>.Success(cost.Id);
+        }
+    }
+}
