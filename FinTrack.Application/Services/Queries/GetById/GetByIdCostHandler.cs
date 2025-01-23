8e81977e4ec99ef14dd63b49dc64a36ae343a898
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-22 22:43:22 -0300

    Dev-Criando GetById

---
+++ FinTrack.Application/Services/Queries/GetById/GetByIdCostHandler.cs
@@ -0,0 +1,31 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Application.Models.CostModel;
+using FinTrack.Core.Entities;
+using FinTrack.Core.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Queries.GetById
+{
+    public class GetByIdCostHandler : IRequestHandler<GetByIdCostQuery, ResultViewModel<CostViewModel>>
+    {
+        private readonly IUoF _Uof;
+
+        public GetByIdCostHandler(IUoF uof)
+        {
+            _Uof = uof;
+        }
+
+        public async Task<ResultViewModel<CostViewModel>> Handle(GetByIdCostQuery request, CancellationToken cancellationToken)
+        {
+            Cost cost =await  _Uof.CostRepository.Get(c=>c.Id == request.CostId);
+            if (cost is null) return ResultViewModel<CostViewModel>.Error("Cost Not Found!");
+            CostViewModel viewModel = CostViewModel.FromEntity(cost);
+            return ResultViewModel<CostViewModel>.Success(viewModel);
+        }
+    }
+}
---
+++ FinTrack.Application/Services/Queries/GetById/GetByIdCostQuery.cs
@@ -0,0 +1,21 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Application.Models.CostModel;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Queries.GetById
+{
+    public class GetByIdCostQuery:IRequest<ResultViewModel<CostViewModel>>
+    {
+        public int CostId { get; set; }
+
+        public GetByIdCostQuery(int costId)
+        {
+            CostId = costId;
+        }
+    }
+}
