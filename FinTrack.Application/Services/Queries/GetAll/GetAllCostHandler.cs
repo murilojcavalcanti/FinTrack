d6fd799f66121d669b442c92957b694478b8d591
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-22 22:43:01 -0300

    Dev-Criando GeatAllCosts

---
+++ FinTrack.Application/Services/Queries/GetAll/GetAllCostHandler.cs
@@ -0,0 +1,29 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Application.Models.CostModel;
+using FinTrack.Core.UnitOfWork;
+using FinTrack.Infrastructure.Persistance.unitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Queries.GetAll
+{
+    public class GetAllCostHandler : IRequestHandler<GetAllCostQuery, ResultViewModel<List<CostViewModel>>>
+    {
+        private readonly IUoF _uoF;
+        public GetAllCostHandler(IUoF uoF)
+        {
+            _uoF = uoF;
+        }
+
+        public async Task<ResultViewModel<List<CostViewModel>>> Handle(GetAllCostQuery request, CancellationToken cancellationToken)
+        {
+            var costs = await _uoF.CostRepository.GetAll();
+            List <CostViewModel> result= costs.Select(cost=>CostViewModel.FromEntity(cost)).ToList();
+            return ResultViewModel<List<CostViewModel>>.Success(result);
+        }
+    }
+}
---
+++ FinTrack.Application/Services/Queries/GetAll/GetAllCostQuery.cs
@@ -0,0 +1,18 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Application.Models.CostModel;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Queries.GetAll
+{
+    public class GetAllCostQuery : IRequest<ResultViewModel<List<CostViewModel>>>
+    {
+        public GetAllCostQuery()
+        {
+        }
+    }
+}
