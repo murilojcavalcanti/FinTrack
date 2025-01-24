99efee18e3a2a21e45c5f0422ed7dd5d52377296
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-23 22:32:36 -0300

    Dev-Criando UpdateReceiveCommand

---
+++ FinTrack.Application/Services/Commands/ReceiveCommands/UpdateReceive/UpdateReceiveCommand.cs
@@ -0,0 +1,25 @@
+﻿using FinTrack.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Commands.ReceiveCommands.UpdateReceive
+{
+    public class UpdateReceiveCommand:IRequest<ResultViewModel>
+    {
+        public UpdateReceiveCommand(string description, decimal valueReceive, int receiveId)
+        {
+            Description = description;
+            ValueReceive = valueReceive;
+            ReceiveId = receiveId;
+        }
+
+        public int ReceiveId { get; set; }
+        public string Description { get; private set; }
+        public decimal ValueReceive { get; private set; }
+
+    }
+}
---
+++ FinTrack.Application/Services/Commands/ReceiveCommands/UpdateReceive/UpdateReceiveHandler.cs
@@ -0,0 +1,34 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Core.Entities;
+using FinTrack.Core.UnitOfWork;
+using FinTrack.Infrastructure.Persistance.unitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Commands.ReceiveCommands.UpdateReceive
+{
+    public class UpdateReceiveHandler : IRequestHandler<UpdateReceiveCommand, ResultViewModel>
+    {
+        private readonly IUoF _uoF;
+
+        public UpdateReceiveHandler(IUoF uoF)
+        {
+            _uoF = uoF;
+        }
+
+        public async Task<ResultViewModel> Handle(UpdateReceiveCommand request, CancellationToken cancellationToken)
+        {
+            Receive receive = await _uoF.ReceiveRepository.Get(r => r.Id == request.ReceiveId);
+            if (receive == null) return ResultViewModel.Error("Receive Not Found!");
+            receive.Update(request.ValueReceive, request.Description);
+            await _uoF.ReceiveRepository.Update(receive);
+            _uoF.Commit();
+            return ResultViewModel.Success();
+        } 
+    }
+}
+ 
