386eb2bfef1da26a169d298648ad472635f6b6f0
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-23 22:30:51 -0300

    Dev-Criando ReceiveViewModel

---
+++ FinTrack.Application/Models/ReceiveModel/ReceiveViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Models.ReceiveModel
+{
+    public class ReceiveViewModel
+    {
+        public ReceiveViewModel(string description, decimal valueReceive)
+        {
+            Description = description;
+            ValueReceive = valueReceive;
+        }
+
+        public string Description { get; private set; }
+        public decimal ValueReceive { get; private set; }
+    }
+}
