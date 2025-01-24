1cc48418373555efbc99f9bafce03611818f10a3
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-23 22:32:22 -0300

    Dev-Criando DeleteReceiveCommand

---
+++ FinTrack.Application/Services/Commands/ReceiveCommands/DeleteReceive/DeleteReceiveCommand.cs
@@ -0,0 +1,20 @@
+﻿using FinTrack.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Commands.ReceiveCommands.DeleteReceive
+{
+    public class DeleteReceiveCommand:IRequest<ResultViewModel>
+    {
+        public int Receiveid { get; private set; }
+
+        public DeleteReceiveCommand(int receiveid)
+        {
+            Receiveid = receiveid;
+        }
+    }
+}
---
+++ FinTrack.Application/Services/Commands/ReceiveCommands/DeleteReceive/DeleteReceiveHandler.cs
@@ -0,0 +1,32 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Core.Entities;
+using FinTrack.Core.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Commands.ReceiveCommands.DeleteReceive
+{
+    public class DeleteReceiveHandler : IRequestHandler<DeleteReceiveCommand, ResultViewModel>
+    {
+        private readonly IUoF _uof;
+
+        public DeleteReceiveHandler(IUoF uof)
+        {
+            _uof = uof;
+        }
+
+        public async Task<ResultViewModel> Handle(DeleteReceiveCommand request, CancellationToken cancellationToken)
+        {
+            Receive receive = await _uof.ReceiveRepository.Get(r=>r.Id==request.Receiveid);
+            if (receive == null) return ResultViewModel.Error("Receive Not Found!");
+            _uof.ReceiveRepository.Delete(receive);
+            _uof.Commit();
+            return ResultViewModel.Success();
+
+        }
+    }
+}
