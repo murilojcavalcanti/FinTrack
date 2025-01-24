553d1d415aef08deabb77bc9a86dc4a44da113bd
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-23 22:34:09 -0300

    Criando ReceiveRepository interface e implementação

---
+++ FinTrack.Infrastructure/Persistance/Repository/ReceiveRepository.cs
@@ -0,0 +1,17 @@
+﻿using FinTrack.Core.Entities;
+using FinTrack.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Infrastructure.Persistance.Repository
+{
+    internal class ReceiveRepository : Repository<Receive>, IReceiveRepository
+    {
+        public ReceiveRepository(FinTrackDbContext context) : base(context)
+        {
+        }
+    }
+}
