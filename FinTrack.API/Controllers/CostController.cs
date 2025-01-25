bd2af8b4b373fd4b0b9cc7ae1b5b4cee21c09cae
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-25 20:41:59 -0300

    Adicionando controller

--- FinTrack.API/Controllers/CostController.cs
+++ FinTrack.API/Controllers/CostController.cs
@@ -20,6 +20,8 @@ namespace FinTrack.API.Controllers
             _mediator = mediator;
         }
 
+
+        // POST: CostController/Create/
         [HttpPost]
         public async Task<IActionResult> CreateCost(CreateCostCommand command)
         {
@@ -28,6 +30,7 @@ namespace FinTrack.API.Controllers
 
             return Ok(result);
         }
+        // GET: CostController/GetAll/
         [HttpGet]
         public async Task<IActionResult> GetAllCost()
         {
@@ -36,23 +39,29 @@ namespace FinTrack.API.Controllers
 
             return Ok(result);
         }
+
+        // GET: CostController/GetByID/
         [HttpGet("/GetById")]
         public async Task<IActionResult> GetByIdCost(GetByIdCostQuery query)
         {
             var result = _mediator.Send(query);
             return Ok(result);
         }
+
+        // GET: CostController/GetByDate
         [HttpGet("/GetByDate")]
         public async Task<IActionResult> GetByDateCost()
         {
             return Ok();
         }
+        // GET: CostController/GetByDescription/
         [HttpGet("/GetByDescription")]
         public async Task<IActionResult> GetByDescritionCost()
         {
             return Ok();
         }
 
+        // PUT: CostController/Update/
         [HttpPut]
         public async Task<IActionResult> UpdateCost(UpdateCostCommand  command)
         {
@@ -60,6 +69,8 @@ namespace FinTrack.API.Controllers
             if (!result.IsSuccess) return BadRequest(result.Messsage);
             return Ok(result);
         }
+
+        // DELETE: CostController/Delete/
         [HttpDelete]
         public async Task<IActionResult> DeleteCost(DeleteCostCommand command)
         {
---
+++ FinTrack.API/Controllers/ReceiveController.cs
@@ -0,0 +1,106 @@
+﻿using FinTrack.Application.Services.Commands.ReceiveCommands.CreateReceive;
+using FinTrack.Application.Services.Commands.ReceiveCommands.DeleteReceive;
+using FinTrack.Application.Services.Commands.ReceiveCommands.UpdateReceive;
+using FinTrack.Application.Services.Queries.ReceiveQueries.GetAll;
+using FinTrack.Application.Services.Queries.ReceiveQueries.GetById;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace FinTrack.API.Controllers
+{
+    public class ReceiveController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ReceiveController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        // POST: ReceiveController/CreateReceive/
+        [HttpPost("/CreateReceive")]
+        public async Task<IActionResult> Create([FromBody]CreateReceiveCommand command)
+        {
+            try
+            {
+                var result = _mediator.Send(command);
+                if (!result.Result.IsSuccess) return BadRequest(result.Result.Messsage);
+                return Ok(result);
+
+            }
+            catch
+            {
+                throw new ArgumentException("Erro ao completar processo");
+            }
+        }
+        
+        // GET: ReceiveController/GetAll/
+        [HttpGet("/GetAllReceive")]
+        public async Task<IActionResult> GetAllReceives()
+        {
+            try
+            {
+                var query = new GetAllReceiveQuery();
+                var result = _mediator.Send(query);
+                if (!result.Result.IsSuccess) return BadRequest(result.Result.Messsage);
+                return Ok(result);
+            }
+            catch
+            {
+                throw new ArgumentException("Erro ao completar processo");
+            }
+        }
+        
+        // GET: ReceiveController/GetById/
+        [HttpGet("/GetByIdReceive")]
+        public async Task<IActionResult> GetByIdReceives([FromBody] GetByIdReceiveQuery query)
+        {
+            try
+            {
+                var result = _mediator.Send(query);
+                if (!result.Result.IsSuccess) return BadRequest(result.Result.Messsage);
+                return Ok(result);
+            }
+            catch
+            {
+                throw new ArgumentException("Erro ao completar processo");
+            }
+        }
+
+        // PUT: ReceiveController/Update/
+        [HttpPut("/UpdateReceive")]
+        public async Task<IActionResult> UpdateReceive([FromBody] UpdateReceiveCommand command)
+        {
+            try
+            {
+                var result = _mediator.Send(command);
+                if (!result.Result.IsSuccess) return BadRequest(result.Result.Messsage);
+                return Ok(result);
+            }
+            catch
+            {
+                throw new ArgumentException("Erro ao completar processo");
+            }
+        }
+
+        // GET: ReceiveController/Delete
+        [HttpDelete("/DeleteReceive")]
+        public async Task<IActionResult> DeleteReceive([FromBody] DeleteReceiveCommand command)
+        {
+            try
+            {
+                var result = _mediator.Send(command);
+                if (!result.Result.IsSuccess) return BadRequest(result.Result.Messsage);
+                return Ok(result);
+            }
+            catch
+            {
+                throw new ArgumentException("Erro ao completar processo");
+            }
+        }
+    }
+}
