47932d02d8233f1a623cbdc9fbf6f7aa8d96e57e
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-25 20:34:15 -0300

    Impementando getAll Receives

---
+++ FinTrack.Application/Services/Queries/ReceiveQueries/GetAll/GetAllReceiveHandler.cs
@@ -0,0 +1,30 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Application.Models.ReceiveModel;
+using FinTrack.Core.Entities;
+using FinTrack.Core.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Queries.ReceiveQueries.GetAll
+{
+    public class GetAllReceiveHandler : IRequestHandler<GetAllReceiveQuery, ResultViewModel<List<ReceiveViewModel>>>
+    {
+        private readonly IUoF _Uof;
+
+        public GetAllReceiveHandler(IUoF uof)
+        {
+            _Uof = uof;
+        }
+
+        public async Task<ResultViewModel<List<ReceiveViewModel>>> Handle(GetAllReceiveQuery request, CancellationToken cancellationToken)
+        {
+            List<Receive> lstReceive =  _Uof.ReceiveRepository.GetAll().Result.ToList();
+            List<ReceiveViewModel> lstReceiveViewModel = lstReceive.Select(r => ReceiveViewModel.FromEntity(r)).ToList();
+            return ResultViewModel<List<ReceiveViewModel>>.Success(lstReceiveViewModel);
+        }
+    }
+}
---
+++ FinTrack.Application/Services/Queries/ReceiveQueries/GetAll/GetAllReceiveQuery.cs
@@ -0,0 +1,15 @@
+﻿using FinTrack.Application.Models.ReceiveModel;
+using FinTrack.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Queries.ReceiveQueries.GetAll
+{
+    public class GetAllReceiveQuery: IRequest<ResultViewModel<List<ReceiveViewModel>>>
+    {
+    }
+}
