2a070ae0e226cc47f68c2b14431559a228f284f8
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-26 19:16:59 -0300

    feature: Mudando pastas

---
+++ FinTrack.Infrastructure/Persistence/FinTrackDbContext.cs
@@ -0,0 +1,19 @@
+﻿using FinTrack.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Infrastructure.Persistence
+{
+    public class FinTrackDbContext : DbContext
+    {
+        public FinTrackDbContext(DbContextOptions<FinTrackDbContext> opts) : base(opts) { }
+
+        public DbSet<Cost> costs { get; set; }
+        public DbSet<Receive> Receives { get; set; }
+        public DbSet<Balance> Balances { get; set; }
+    }
+}
---
+++ FinTrack.Infrastructure/Persistence/unitOfWork/UoF.cs
@@ -0,0 +1,56 @@
+﻿using FinTrack.Core.Repositories;
+using FinTrack.Core.UnitOfWork;
+using FinTrack.Infrastructure.Persistence.Repository;
+
+namespace FinTrack.Infrastructure.Persistence.unitOfWork
+{
+    public class UoF : IUoF
+    {
+        private readonly FinTrackDbContext _context;
+
+        private IUserRepository _userRepository;
+        private ICostRepository _costRepository;
+        private IReceiveRepository _ReceiveRepository;
+        private IBalanceRepository _BalanceRepository;
+        public UoF(FinTrackDbContext context)
+        {
+            _context = context;
+        }
+
+        public IUserRepository UserRepository
+        {
+            get
+            {
+                return _userRepository = _userRepository ?? new UserRepository(_context);
+            }
+        }
+
+        public ICostRepository CostRepository
+        {
+            get
+            {
+                return _costRepository = _costRepository ?? new CostRepository(_context);
+            }
+        }
+
+        public IReceiveRepository ReceiveRepository
+        {
+            get
+            {
+                return _ReceiveRepository ?? new ReceiveRepository(_context);
+            }
+        }
+        public IBalanceRepository BalanceRepository
+        {
+            get
+            {
+                return _BalanceRepository ?? new BalanceRepository(_context);
+            }
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
