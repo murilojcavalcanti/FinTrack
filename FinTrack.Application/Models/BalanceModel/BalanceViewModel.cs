ada45e47fe1f87285c0e83f6da7053337c1c5647
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-26 19:18:20 -0300

    Dev: Criando viewModel

---
+++ FinTrack.Application/Models/BalanceModel/BalanceViewModel.cs
@@ -0,0 +1,23 @@
+﻿using FinTrack.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Models.BalanceModel
+{
+    public class BalanceViewModel
+    {
+        public BalanceViewModel(int month, int year)
+        {
+            Month = month;
+            Year = year;
+        }
+
+        public int Month { get; private set; }
+        public int Year { get; private set; }
+
+        public static BalanceViewModel FromEntity(Balance balance ) => new(balance.Month, balance.Year);
+    }
+}
