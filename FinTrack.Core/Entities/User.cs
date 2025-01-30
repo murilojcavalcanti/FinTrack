c48c8e17db1d53eba9b9bd18f5650c0f6c7a7ce0
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-30 18:36:23 -0300

    Criando update na entidade user

--- FinTrack.Core/Entities/User.cs
+++ FinTrack.Core/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,5 +19,11 @@ namespace FinTrack.Core.Entities
         public string Name { get; private set; }
         public string Email { get; private set; }
         public string Password { get; private set; }
+        public void Update(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
     }
 }
