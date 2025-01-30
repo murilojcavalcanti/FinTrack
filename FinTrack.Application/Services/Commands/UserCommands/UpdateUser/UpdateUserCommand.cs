397d2f1eb6ad6cf64ec48848018b5576a3b6e042
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-30 18:37:38 -0300

    Criando Update user command

---
+++ FinTrack.Application/Services/Commands/UserCommands/UpdateUser/UpdateUserCommand.cs
@@ -0,0 +1,26 @@
+﻿using FinTrack.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Commands.UserCommands.UpdateUser
+{
+    public class UpdateUserCommand:IRequest<ResultViewModel>
+    {
+        public UpdateUserCommand(string name, string email, string password, int userId)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            this.userId = userId;
+        }
+
+        public int userId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ FinTrack.Application/Services/Commands/UserCommands/UpdateUser/UpdateUserHandler.cs
@@ -0,0 +1,32 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Core.Entities;
+using FinTrack.Core.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Commands.UserCommands.UpdateUser
+{
+    public class UpdateUserHandler : IRequestHandler<UpdateUserCommand, ResultViewModel>
+    {
+        private readonly IUoF _uoF;
+
+        public UpdateUserHandler(IUoF uoF)
+        {
+            _uoF = uoF;
+        }
+
+        public async Task<ResultViewModel> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            User user = await _uoF.UserRepository.Get(u=>u.Id==request.userId);
+            if (user == null) return ResultViewModel.Error("User Not Found");
+            user.Update(request.Name,request.Email,request.Password);
+            await _uoF.UserRepository.Update(user);
+            _uoF.Commit();
+            return ResultViewModel.Success();
+        }
+    }
+}
