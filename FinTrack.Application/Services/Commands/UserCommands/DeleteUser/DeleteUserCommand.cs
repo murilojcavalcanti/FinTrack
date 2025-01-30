506a2ee20f6dd3ec8a19e3bcec1e1e3fadbeb0b7
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-30 18:37:24 -0300

    Criando Delete user command

---
+++ FinTrack.Application/Services/Commands/UserCommands/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,15 @@
+﻿using FinTrack.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Commands.UserCommands.DeleteUser
+{
+    public class DeleteUserCommand:IRequest<ResultViewModel>
+    {
+        public int UserId { get; set; }
+    }
+}
---
+++ FinTrack.Application/Services/Commands/UserCommands/DeleteUser/DeleteUserHandler.cs
@@ -0,0 +1,33 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Core.Entities;
+using FinTrack.Core.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Commands.UserCommands.DeleteUser
+{
+    public class DeleteUserHandler : IRequestHandler<DeleteUserCommand, ResultViewModel>
+    {
+        public readonly IUoF _uof;
+
+        public DeleteUserHandler(IUoF uof)
+        {
+            _uof = uof;
+        }
+
+        public async Task<ResultViewModel> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            User user = await _uof.UserRepository.Get(u=>u.Id == request.UserId);
+            
+            if (user == null) return ResultViewModel.Error("User Not Found");
+            
+            await _uof.UserRepository.Delete(user);
+            _uof.Commit(); 
+            return ResultViewModel.Success();
+        }
+    }
+}
