6d630992eca829212f57fb9ebe2a5f857ec33c2b
murilojcavalcanti <josemurilo7@gmail.com>
2025-01-30 18:37:14 -0300

    Criando create user command

---
+++ FinTrack.Application/Services/Commands/UserCommands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,27 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Commands.UserCommands.CreateUser
+{
+    public class CreateUserCommand:IRequest<ResultViewModel<int>>
+    {
+        public CreateUserCommand(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User ToEntity() => new(Name, Email, Password);
+    }
+}
---
+++ FinTrack.Application/Services/Commands/UserCommands/CreateUser/CreateUserHandler.cs
@@ -0,0 +1,32 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Core.Entities;
+using FinTrack.Core.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Commands.UserCommands.CreateUser
+{
+    public class CreateUserHandler : IRequestHandler<CreateUserCommand, ResultViewModel<int>>
+    {
+        private readonly IUoF _uof;
+
+        public CreateUserHandler(IUoF uof)
+        {
+            _uof = uof;
+        }
+
+        public async Task<ResultViewModel<int>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            User user = request.ToEntity();
+            bool userIsNull =await  _uof.UserRepository.Get(u => u.Email == user.Email) is null;
+            if (!userIsNull) return ResultViewModel<int>.Error("registered user!");
+            await _uof.UserRepository.Insert(user);
+            _uof.Commit();
+            return ResultViewModel<int>.Success(user.Id);
+        }
+    }
+}
