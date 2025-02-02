0b2c369d4ce55cd6bc7afd514302cc353c228c30
murilojcavalcanti <josemurilo7@gmail.com>
2025-02-02 17:13:04 -0300

    Criando GetById de User

---
+++ FinTrack.Application/Services/Queries/UsersQueries/GetByIdUsersQuery/GetByIdUserHandler.cs
@@ -0,0 +1,30 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Application.Models.UserModel;
+using FinTrack.Core.Entities;
+using FinTrack.Core.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Queries.UsersQueries.GetByIdUsersQuery
+{
+    public class GetByIdUserHandler : IRequestHandler<GetByIdUserQuery, ResultViewModel<UserViewModel>>
+    {
+        private readonly IUoF _uof;
+
+        public GetByIdUserHandler(IUoF uof)
+        {
+            _uof = uof;
+        }
+
+        public async Task<ResultViewModel<UserViewModel>> Handle(GetByIdUserQuery request, CancellationToken cancellationToken)
+        {
+            User user = await _uof.UserRepository.Get(u=>u.Id==request.UserId);
+            UserViewModel result = UserViewModel.FromEntity(user);
+            return ResultViewModel<UserViewModel>.Success(result);
+        }
+    }
+}
---
+++ FinTrack.Application/Services/Queries/UsersQueries/GetByIdUsersQuery/GetByIdUserQuery.cs
@@ -0,0 +1,21 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Application.Models.UserModel;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Queries.UsersQueries.GetByIdUsersQuery
+{
+    public  class GetByIdUserQuery: IRequest<ResultViewModel<UserViewModel>>
+    {
+        public int UserId { get; set; }
+
+        public GetByIdUserQuery(int userId)
+        {
+            UserId = userId;
+        }
+    }
+}
