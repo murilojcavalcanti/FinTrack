8a0387c09384b3f73bcc72d4b7685591700cadeb
murilojcavalcanti <josemurilo7@gmail.com>
2025-02-02 17:12:23 -0300

    Criando controller de usuario

--- FinTrack.API/Controllers/UserController.cs
+++ FinTrack.API/Controllers/UserController.cs
@@ -1,4 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FinTrack.Application.Services.Commands.ReceiveCommands.CreateReceive;
+using FinTrack.Application.Services.Commands.ReceiveCommands.DeleteReceive;
+using FinTrack.Application.Services.Commands.ReceiveCommands.UpdateReceive;
+using FinTrack.Application.Services.Commands.UserCommands.CreateUser;
+using FinTrack.Application.Services.Commands.UserCommands.DeleteUser;
+using FinTrack.Application.Services.Commands.UserCommands.UpdateUser;
+using FinTrack.Application.Services.Queries.ReceiveQueries.GetAll;
+using FinTrack.Application.Services.Queries.ReceiveQueries.GetById;
+using FinTrack.Application.Services.Queries.UsersQueries.GetAllUsersQuery;
+using FinTrack.Application.Services.Queries.UsersQueries.GetByIdUsersQuery;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FinTrack.API.Controllers
 {
@@ -6,6 +17,93 @@ namespace FinTrack.API.Controllers
     [Route("[Controller]")]
     public class UserController:ControllerBase
     {
+        private readonly IMediator _mediator;
 
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        // POST: UserController/CreateUser/
+        [HttpPost("/CreateUser")]
+        public async Task<IActionResult> CreateUser([FromBody] CreateUserCommand command)
+        {
+            try
+            {
+                var result = _mediator.Send(command);
+                if (!result.Result.IsSuccess) return BadRequest(result.Result.Messsage);
+                return Ok(result.Result);
+            }
+            catch
+            {
+                throw new ArgumentException("Erro ao completar processo");
+            }
+        }
+
+        // GET: UserController/GetAllUser/
+        [HttpGet("/GetAllUser")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            try
+            {
+                var query = new GetAllUsersQuery();
+                var result = await _mediator.Send(query);
+                if (!result.IsSuccess) return BadRequest(result.Messsage);
+                return Ok(result.Data);
+            }
+            catch
+            {
+                throw new ArgumentException("Erro ao completar processo");
+            }
+        }
+
+        // GET: UserController/GetByIdUser/
+        [HttpGet("/GetByIdUser")]
+        public async Task<IActionResult> GetByIdReceives([FromBody] GetByIdUserQuery query)
+        {
+            try
+            {
+                var result = await _mediator.Send(query);
+                if (!result.IsSuccess) return BadRequest(result.Messsage);
+                return Ok(result.Data);
+            }
+            catch
+            {
+                throw new ArgumentException("Erro ao completar processo");
+            }
+        }
+
+        // PUT: UserController/UpdateUser/
+        [HttpPut("/UpdateUser")]
+        public async Task<IActionResult> UpdateUser([FromBody] UpdateUserCommand command)
+        {
+            try
+            {
+                var result = await _mediator.Send(command);
+                if (!result.IsSuccess) return BadRequest(result.Messsage);
+                return Ok(result.IsSuccess);
+            }
+            catch
+            {
+                throw new ArgumentException("Erro ao completar processo");
+            }
+        }
+
+        // GET: UserController/DeleteUser
+        [HttpDelete("/DeleteUser")]
+        public async Task<IActionResult> DeleteUser([FromBody] DeleteUserCommand command)
+        {
+            try
+            {
+                var result = await _mediator.Send(command);
+                if (!result.IsSuccess) return BadRequest(result.Messsage);
+                return Ok(result.IsSuccess);
+            }
+            catch
+            {
+                throw new ArgumentException("Erro ao completar processo");
+            }
+        }
     }
 }
