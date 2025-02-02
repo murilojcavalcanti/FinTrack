efde40465b5aa66d34d0bac206bc94762b6ca216
murilojcavalcanti <josemurilo7@gmail.com>
2025-02-02 17:12:53 -0300

    Criando getallUser

---
+++ FinTrack.Application/Services/Queries/UsersQueries/GetAllUsersQuery/GetAllUsersHandler.cs
@@ -0,0 +1,30 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Application.Models.UserModel;
+using FinTrack.Core.Entities;
+using FinTrack.Core.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Queries.UsersQueries.GetAllUsersQuery
+{
+    public class GetAllUsersHandler : IRequestHandler<GetAllUsersQuery, ResultViewModel<List<UserViewModel>>>
+    {
+        private readonly IUoF _uof;
+
+        public GetAllUsersHandler(IUoF uof)
+        {
+            _uof = uof;
+        }
+
+        public async Task<ResultViewModel<List<UserViewModel>>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<User> users = await _uof.UserRepository.GetAll();
+            List<UserViewModel> result = users.Select(u=>UserViewModel.FromEntity(u)).ToList();
+            return ResultViewModel<List<UserViewModel>>.Success(result);
+        }
+    }
+}
---
+++ FinTrack.Application/Services/Queries/UsersQueries/GetAllUsersQuery/GetAllUsersQuery.cs
@@ -0,0 +1,15 @@
+﻿using FinTrack.Application.Models;
+using FinTrack.Application.Models.UserModel;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Services.Queries.UsersQueries.GetAllUsersQuery
+{
+    public  class GetAllUsersQuery:IRequest<ResultViewModel<List<UserViewModel>>>
+    {
+    }
+}
