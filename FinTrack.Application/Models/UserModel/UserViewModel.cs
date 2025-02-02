af0ad6afcb697fb4d5539024b3173b55f32e0aa9
murilojcavalcanti <josemurilo7@gmail.com>
2025-02-02 17:12:42 -0300

    Criando userViewModel

---
+++ FinTrack.Application/Models/UserModel/UserViewModel.cs
@@ -0,0 +1,29 @@
+﻿using FinTrack.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Application.Models.UserModel
+{
+    public class UserViewModel
+    {
+        public UserViewModel(int id, string name, string email, DateTime createdAt, DateTime updatedAt)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public static UserViewModel FromEntity(User user)=> new (user.Id,user.Name,user.Email,user.CreatedAt,user.UpdatedAt);
+    }
+}
