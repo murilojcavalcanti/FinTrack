505d2259520b273b481e5e4441b112b1d9193380
murilojcavalcanti <josemurilo7@gmail.com>
2025-02-12 23:10:58 -0300

    Atualizando o create balance

--- FinTrack.Application/Services/Commands/BalanceCommands/CreateBalance/CreateBalanceCommand.cs
+++ FinTrack.Application/Services/Commands/BalanceCommands/CreateBalance/CreateBalanceCommand.cs
@@ -12,16 +12,18 @@ namespace FinTrack.Application.Services.Commands.BalanceCommands.CreateBalance
 {
     public class CreateBalanceCommand:IRequest<ResultViewModel<int>>
     {
-        public CreateBalanceCommand(int month, int year)
+        public CreateBalanceCommand(int month, int year, int userId)
         {
             Month = month;
             Year = year;
+            UserId = userId;
         }
 
         [Range(1,12,ErrorMessage ="Os meses do ano vão de 1 a 12")]
         public int Month { get; set; }
         public int Year { get; set; }
+        public int UserId { get; set; }
 
-        public Balance ToEntity() => new Balance(Month,Year);
+        public Balance ToEntity() => new Balance(Month,Year,UserId);
     }
 }
