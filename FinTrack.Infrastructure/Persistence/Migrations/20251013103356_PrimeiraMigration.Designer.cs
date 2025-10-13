af4451342c43efdb184d5e326c3b1931fbe3e924
Murilo Cavalcanti <josemurilo7@gmail.com>
2025-10-13 08:02:48 -0300

    subindo migration

--- FinTrack.Infrastructure/Persistence/Migrations/20251013103356_PrimeiraMigration.Designer.cs
+++ FinTrack.Infrastructure/Persistence/Migrations/20251013103356_PrimeiraMigration.Designer.cs
@@ -4,6 +4,7 @@ using FinTrack.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FinTrack.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(FinTrackDbContext))]
-    partial class FinTrackDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251013103356_PrimeiraMigration")]
+    partial class PrimeiraMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
